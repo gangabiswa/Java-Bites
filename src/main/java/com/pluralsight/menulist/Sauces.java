1de907cc3993840061c859149aa83b0cce4aa4de
gangabiswa <Gbiswa@pgh.year.org>
2024-05-30 10:45:42 -0400

    stared working in userinterface

---
+++ src/main/java/com/pluralsight/menulist/Sauces.java
@@ -0,0 +1,17 @@
+package com.pluralsight.menulist;
+
+public class Sauces {
+    private String Name;
+
+    public Sauces(String name) {
+        Name = name;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+}
--- src/main/java/com/pluralsight/menulist/Userinterface.java
+++ src/main/java/com/pluralsight/menulist/Userinterface.java
@@ -1,7 +1,50 @@
 package com.pluralsight.menulist;
 
+import java.util.Scanner;
+
 public class Userinterface {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int Options;
 
+        do {
+            System.out.println("1) Home");
+            System.out.println("0) Exit");
+            Options = scanner.nextInt();
+             scanner.nextLine();
+
+             switch (Options){
+                 case 1:
+                     creatSandwich(scanner);
+                     break;
+
+             }
+        }while (Options !=0);
+
+    }
+    public  static  void creatSandwich(Scanner scanner){
+        System.out.println( "select break type");
+        System.out.println("1) white");
+        System.out.println("2) wheat");
+        System.out.println("3) rye");
+        System.out.println("4) wrap");
+        String BreadType = "";
+        switch (scanner.nextInt()) {
+            case 1:
+                BreadType = "White";
+                break;
+            case 2:
+                BreadType = "Wheat";
+                break;
+            case 3:
+                BreadType = "Rye";
+                break;
+            case 4:
+                BreadType = "Wrap";
+                break;
+            default:
+                BreadType = "white";
+        }
+        scanner.nextLine();
     }
 }
